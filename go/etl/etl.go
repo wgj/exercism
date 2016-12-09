e40ad1a503f6ce68acf5898fc847558206cc1319
Wes Johnson <wes@futureprefect.com>
2016-12-08 19:54:22 -0800

    etl solution

---
+++ go/etl/etl.go
@@ -0,0 +1,20 @@
+package etl
+
+import (
+	"strings"
+)
+
+func Transform(m map[int][]string) map[string]int {
+	results := make(map[string]int)
+	// for each key in m
+	for k, v := range m {
+		// for each value in key
+		for _, letter := range v {
+			// insert results[value]key
+			results[strings.ToLower(letter)] = k
+		}
+	}
+
+	return results
+
+}
